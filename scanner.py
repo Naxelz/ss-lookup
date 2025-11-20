70b5416746947d270c750d4f02d23db04afefe60
Naxel´z <ronaldotps854@gmail.com>
2025-11-20 11:41:29 -0300

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -14,6 +14,8 @@ import shutil
 import tempfile
 import glob
 
+Path('reportes').mkdir(exist_ok=True)
+
 logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(levelname)s - %(message)s',
